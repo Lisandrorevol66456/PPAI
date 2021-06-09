325d6b4ac55c06a0b0c111a35757e679e7387ab0
pavIutn <pavIutn@hotmail.com>
2021-06-09 09:25:43 -0300

    boundary - controller - entity

---
+++ Museo-pictorico_ppai/Modelos/Entrada.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Museo_pictorico_ppai.Modelos
+{
+    class Entrada
+    {
+        public int idEntrada { get; set; }
+        public int idSede { get; set; }
+        public int tipoEntrada { get; set; }
+        public bool sservicioGuia { get; set; }
+    }
+}
